5d596c3e1a2c2c7927cc9d58ab3f434207413721
xuqingkai <xuqingkai@hd123.com>
2023-05-25 23:13:11 +0800

    fix: 🐛 移除一些打印信息

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: weisheng
  * @Date: 2023-05-24 18:44:55
- * @LastEditTime: 2023-05-25 22:34:10
+ * @LastEditTime: 2023-05-25 23:12:39
  * @LastEditors: weisheng
  * @Description:
  * @FilePath: \uni-mini-ci\src\utils\Config.ts
@@ -28,7 +28,5 @@ export async function getConfig(): Promise<CIOptions> {
   const packageJson = JSON5.parse(packageSource)
   data.version = data.version ? data.version : packageJson.version
   data.desc = data.desc ? data.desc : packageJson.description
-  console.log(data)
-
   return data
 }
