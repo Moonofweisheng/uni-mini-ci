8dcf22042f2b6a6d680157629c37db9a85721f56
xuqingkai <xuqingkai@hd123.com>
2023-08-03 12:39:32 +0800

    fix: 🐛 修复读取配置文件错误时没有结束当前进程的问题

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: weisheng
  * @Date: 2023-05-24 18:44:55
- * @LastEditTime: 2023-05-25 23:12:39
+ * @LastEditTime: 2023-08-03 12:38:35
  * @LastEditors: weisheng
  * @Description:
  * @FilePath: \uni-mini-ci\src\utils\Config.ts
@@ -19,7 +19,13 @@ joycon.addLoader({
   test: /\.minicirc$/,
   load(filePath: string) {
     const source = fs.readFileSync(filePath, 'utf-8')
-    return JSON5.parse(source)
+    try {
+      const minicirc = JSON5.parse(source)
+      return minicirc
+    } catch (error) {
+      console.error(`读取.minicirc文件失败 ${new Date().toLocaleString()} \n${error}`)
+      process.exit(1)
+    }
   }
 })
 export async function getConfig(): Promise<CIOptions> {
